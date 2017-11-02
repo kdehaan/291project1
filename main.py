ade3197bfd4c69ec1dd6125ff431780d08ff6c04
Kevin de Haan <kbehaan@gmail.com>
2017-11-01 21:03:17 -0600

    documentation and read from file function

--- main.py
+++ main.py
@@ -1,21 +1,45 @@
 import sqlite3
-import tables
+from sqlite3 import OperationalError
+
+conn = sqlite3.connect('./data.db')
+sql = conn.cursor();
 
 
-def create_connection():
-    return sqlite3.connect('./data.db')
+def sql_from_file(filename):
+    """
+    Adapted from https://stackoverflow.com/a/19473206
+    Runs sql queries from a .sql or .txt file
+    :param filename:
+    :return:
+    """
+    f = open(filename, 'r')
+    sql_file = f.read()
+    f.close()
+    commands = sql_file.split(';')
+    for command in commands:
+        try:
+            sql.execute(command)
+        except OperationalError:
+            print("Command skipped")
+    conn.commit()
 
 
 def setup():
-    conn = create_connection()
-    sql = conn.cursor();
+    """
+    Setup function
+    :return:
+    """
     sql.execute('''PRAGMA foreign_keys=ON;''')
-    tables.create_tables(conn, sql)
+    sql_from_file('./tables.sql')
 
     conn.commit()
 
 
 def main():
+    """
+    Main function
+    :return:
+    """
     setup()
 
 
