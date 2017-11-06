4cbf52fbf7ece49890bb1c5a4bf05cd66ca98ab1
Kevin de Haan <kbehaan@gmail.com>
2017-11-05 20:32:59 -0700

    login issue fixed

--- interface.py
+++ interface.py
@@ -519,7 +519,7 @@ class Interface:
                             where c.cid=:cid
                             and c.pwd=:pwd''',
                          {'cid': cid, 'pwd': password})
-        response = self.sql.fetchall()[0]
+        response = self.sql.fetchall()
         if not response:
             print("Invalid ID or Password")
             time.sleep(self.delay)
@@ -529,7 +529,7 @@ class Interface:
             else:
                 return 'c'
         else:
-            print('-- Logged in as ' + response[0] + ' --')
+            print('-- Logged in as ' + response[0][0] + ' --')
             self.userID = cid
             return 'cm'
 
